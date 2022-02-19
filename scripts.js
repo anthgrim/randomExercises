540b305a2ddef7881fddf9f470df261e790548b0
anthgrim <anthgrim@icloud.com>
2022-02-19 17:10:51 -0500

    Removing blank lines from js

--- scripts.js
+++ scripts.js
@@ -13,26 +13,21 @@ const isString = (array) => {
 
 function arrayFlattener(pArray){
     let odArray = [];
-
     //Get the second dimension flat
     for(let i = 0; i < pArray.length; i++){
         if(pArray[i].length > 1){
             for(let j = 0; j < pArray[i].length; j++){
                 odArray.push(pArray[i][j]);
             }
-
             //Evaluate if the recently added elements have more than one dimension each
             for(let k = 0; k < odArray.length; k ++){
                 if(odArray[k].length > 1){
                     odArray = arrayFlattener(odArray)
                 }
             }
-            
         }else{
             odArray.push(pArray[i]);
         }
-
-        
     }
     return odArray;
 }
