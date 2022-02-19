14eda497973dcf403f8f4a424d80ac4fe247c964
anthgrim <anthgrim@icloud.com>
2022-02-19 16:48:31 -0500

    Array flattener function, for multidimensional arrays

--- scripts.js
+++ scripts.js
@@ -10,3 +10,29 @@ const isString = (array) => {
     }
     return 'strings';
 }
+
+function arrayFlattener(pArray){
+    let odArray = [];
+
+    //Get the second dimension flat
+    for(let i = 0; i < pArray.length; i++){
+        if(pArray[i].length > 1){
+            for(let j = 0; j < pArray[i].length; j++){
+                odArray.push(pArray[i][j]);
+            }
+
+            //Evaluate if the recently added elements have more than one dimension each
+            for(let k = 0; k < odArray.length; k ++){
+                if(odArray[k].length > 1){
+                    odArray = arrayFlattener(odArray)
+                }
+            }
+            
+        }else{
+            odArray.push(pArray[i]);
+        }
+
+        
+    }
+    return odArray;
+}
